b78d9796af844b307ede7220c2136b8271bd6a30
Gabriel <gabriel_corbani@hotmail.com>
2020-11-20 20:06:18 -0300

    Arreglo timer en level clear
    Solucion horripilante, sepan entender (?

--- Race Track/scenes/screens/levelClear.gd
+++ Race Track/scenes/screens/levelClear.gd
@@ -4,5 +4,11 @@ func _ready():
 	pass # Replace with function body.
 
 func Set_UltimoTiempo(value):
-	$Label.set_text( Globals.get_StrTiempo(value))
+	#$Label.set_text( Globals.get_StrTiempo(value))
+	if Globals.current_level == 0:
+		$Label.set_text( Globals.get_StrTiempo(Globals.tiempo_level1 ))
+	if Globals.current_level == 1:
+		$Label.set_text( Globals.get_StrTiempo(Globals.tiempo_level2 ))
+	if Globals.current_level == 2:
+		$Label.set_text( Globals.get_StrTiempo(Globals.tiempo_level3 ))
 
