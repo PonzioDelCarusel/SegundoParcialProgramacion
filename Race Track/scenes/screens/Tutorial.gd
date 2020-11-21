a81fc173a55b339a66ff5c48457d3bbaedb7571a
Gabriel <gabriel_corbani@hotmail.com>
2020-11-21 01:59:52 -0300

    Pantalla tutorial
    Agregada pantalla tuto para mostrar controles

---
+++ Race Track/scenes/screens/Tutorial.gd
@@ -0,0 +1,26 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+# warning-ignore:return_value_discarded
+	connect("pressed", self, "_on_button_pressed")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+export(String) var target_scene = ""
+
+func _on_button_pressed():
+	var salirErr = get_tree().change_scene(target_scene)
+	if salirErr != OK:
+		print("Error al cargar la escena")
+	pass # Replace with function body.
